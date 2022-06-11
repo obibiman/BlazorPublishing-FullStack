9f7ab140282bd675c5a68dc58ce4f329fcf5d581
Maurice Muoneke <maurice.muoneke@outlook.com>
2022-06-11 12:22:23 -0500

    Add project files.

---
+++ Blazor.SankoreAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor.SankoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("making call to weather endpoint");
+            try
+            {
+                throw new NotImplementedException();
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+            }
+            catch(Exception exep)
+            {
+                _logger.LogError(exep, exep.Message);
+                throw;
+            }
+            finally
+            {
+                _logger.LogInformation("This will always be called");
+            }         
+        }
+    }
+}
